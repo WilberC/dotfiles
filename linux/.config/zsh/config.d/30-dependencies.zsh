0fe175b0e6407054c33a4952c6daea8b829a7f5c
Wilber Carrascal <wilber.carrascal@gmail.com>
2026-04-05 01:06:18 -0500

    Add Homebrew installation check to dependencies script for Linux and WSL2

--- linux/.config/zsh/config.d/30-dependencies.zsh
+++ linux/.config/zsh/config.d/30-dependencies.zsh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
+# Homebrew (installed at /home/linuxbrew on Linux/WSL2)
+[[ -x /home/linuxbrew/.linuxbrew/bin/brew ]] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 # Use 1Password auth sock
 export SSH_AUTH_SOCK=~/.1password/agent.sock
