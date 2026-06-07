f8daa44799aa84263d0a55c903b8873f20cce879
Wilber Carrascal <wilber.carrascal@gmail.com>
2026-06-07 02:14:17 -0500

    Add WSL Zone.Identifier watcher

--- shared/.config/fish/config.fish
+++ shared/.config/fish/config.fish
@@ -10,3 +10,15 @@ set -g fish_greeting
 if test (uname) = Darwin
   source ~/.orbstack/shell/init2.fish 2>/dev/null || true
 end
+
+fish_add_path -g "$HOME/.local/bin"
+
+function clean-zone-watch
+  set -l watch_dir "$PWD"
+  if set -q argv[1]
+    set watch_dir "$argv[1]"
+  end
+
+  mkdir -p "$HOME/.cache"
+  nohup watch-zone-identifiers "$watch_dir" > "$HOME/.cache/watch-zone-identifiers.log" 2>&1 &
+end
